acc468ff4a438d44c842e3a1097f320e837b50fd
Dima Shamoev <dimashamoev5@gmail.com>
2024-11-27 16:52:49 +0400

    added filter UI in Product page

--- src/Pages/Product/Product.tsx
+++ src/Pages/Product/Product.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "./Product.css"
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 const Product: React.FunctionComponent = () => {
     interface IProductVehicles {
@@ -134,12 +136,104 @@ const Product: React.FunctionComponent = () => {
             <div className="product-page-filter-list">
 
                 <div className="product-page-filter">
-                    <div className="filter-location-input">
-                        
+                    <div className="product-page-filter-wrapper">
+
+                        <div className="filter-location-input filter-input-block">
+                            <span className="location-span filter-input-block-span">Location</span>
+                            <div className="filter-location-placeholder filter-input-block-placeholder">
+                                <span>New-York</span>
+                                <span><MdKeyboardArrowDown /></span>
+                            </div>
+                        </div>
+
+                        <div className="filter-search-zip-inputs-block">
+                            <div className="filter-search-input filter-input-block">
+                                <span className="location-span search-span">Search Within</span>
+                                <div className="filter-search-placeholder filter-input-block-placeholder">
+                                    <span>200 Miles</span>
+                                    <span><MdKeyboardArrowDown /></span>
+                                </div>
+                            </div>
+                            <div className="filter-zip-input filter-input-block">
+                                <span className="filter-input-block-span">Zip Code</span>
+                                <div className="filter-zip-placeholder filter-input-block-placeholder">
+                                    <span>02111</span>
+                                    <span><MdKeyboardArrowDown /></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="filter-condition-input filter-input-block">
+                            <span className="filter-input-block-span">Condition</span>
+                            <div className="filter-condition-placeholder filter-input-block-placeholder">
+                                <span>New And Used</span>
+                                <span><MdKeyboardArrowDown /></span>
+                            </div>
+                        </div>
+
+                        <hr />
+
+                        <div className="filter-car-type">
+                            <span>Type</span>
+                            <ul>
+                                <li><input type="checkbox" /> SUV</li>
+                                <li><input type="checkbox" /> Sedan</li>
+                                <li><input type="checkbox" /> Hatchback</li>
+                                <li><input type="checkbox" /> Coupe</li>
+                                <li><input type="checkbox" /> Convertible</li>
+                            </ul>
+                        </div>
+
+                        <hr />
+
+                        <div className="filter-price-block">
+                            <div className="filter-price-title">
+                                Price
+                            </div>
+                            <div className="price-cells">
+                                <div className="min-price">
+                                    <span>Min. Price</span>
+                                    <div>$5000</div>
+                                </div>
+                                <div className="max-price">
+                                    <span>Max. Price</span>
+                                    <div>$7000</div>
+                                </div>
+                            </div>
+                            <div className="price-range">
+                                <input type="range" />
+                            </div>
+                        </div>
+
+                        <hr />
+
+                        <div className="filter-gear-type filter-car-type">
+                            <span>Transmission</span>
+                            <ul>
+                                <li><input type="checkbox" /> Diesel</li>
+                                <li><input type="checkbox" /> Diesel</li>
+                                <li><input type="checkbox" /> Diesel</li>
+                            </ul>
+                        </div>
+
+                        <hr />
+
+                        <div className="filter-fuel-type filter-car-type">
+                            <span>Fuel Type</span>
+                            <ul>
+                                <li><input type="checkbox" /> Diesel</li>
+                                <li><input type="checkbox" /> Diesel</li>
+                                <li><input type="checkbox" /> Diesel</li>
+                                <li><input type="checkbox" /> Diesel</li>
+                            </ul>
+                        </div>
+
                     </div>
                 </div>
 
-                <div className="product-page-cars"></div>
+                <div className="product-page-cars">
+
+                </div>
             </div>
         </div>
     );
