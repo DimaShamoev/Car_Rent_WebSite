63392bcb5d4d145097cb47bd04b620dcc4c9be5b
Dima Shamoev <dimashamoev5@gmail.com>
2024-11-29 00:33:41 +0400

    finished pricing page

--- src/Pages/Pricing/Pricing.tsx
+++ src/Pages/Pricing/Pricing.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "./Pricing.css"
+import { FaCheck } from "react-icons/fa";
 
 const Pricing: React.FunctionComponent = () => {
     interface IPricingOffers {
@@ -14,7 +16,7 @@ const Pricing: React.FunctionComponent = () => {
             id: 1,
             price: 29,
             name: "Basic Plan",
-            desc: "Offers Description",
+            desc: "Quis autem vel eum iure reprehenderit qui in ea voluptate velit.",
             offers: {
                 offer_1: "listing 1",
                 offer_2: "30 Days Visibility",
@@ -28,7 +30,7 @@ const Pricing: React.FunctionComponent = () => {
             id: 2,
             price: 49,
             name: "Standard Plan",
-            desc: "Offers Description",
+            desc: "Quis autem vel eum iure reprehenderit qui in ea voluptate velit.",
             offers: {
                 offer_1: "listing 1",
                 offer_2: "30 Days Visibility",
@@ -42,7 +44,7 @@ const Pricing: React.FunctionComponent = () => {
             id: 3,
             price: 89,
             name: "Extended Plan",
-            desc: "Offers Description",
+            desc: "Quis autem vel eum iure reprehenderit qui in ea voluptate velit.",
             offers: {
                 offer_1: "listing 1",
                 offer_2: "30 Days Visibility",
@@ -56,7 +58,7 @@ const Pricing: React.FunctionComponent = () => {
             id: 4,
             price: 129,
             name: "Enterprise Plan",
-            desc: "Offers Description",
+            desc: "Quis autem vel eum iure reprehenderit qui in ea voluptate velit.",
             offers: {
                 offer_1: "listing 1",
                 offer_2: "30 Days Visibility",
@@ -68,22 +70,56 @@ const Pricing: React.FunctionComponent = () => {
         },
     ];
 
+    const [cardHoverActive, setCardHoverActive] = React.useState<number | null>(null)
+
+    const handleActiveHover = (index: number) => {
+        setCardHoverActive(index)
+    }
+
+    const handleDeactiveHover = () => {
+        setCardHoverActive(null)
+    }
+
     return (
         <div className="pricing-page">
             <div className="pricing-title">Membership Plans</div>
             <div className="pricing-offers">
                 {PricingOffers.map((offer) => (
-                    <div className="offer-block" key={offer.id}>
-                        <p>${offer.price}</p>
-                        <p>{offer.name}</p>
-                        <p>{offer.desc}</p>
-                        <p>{offer.offers.offer_1}</p>
-                        <p>{offer.offers.offer_2}</p>
-                        <p>{offer.offers.offer_3}</p>
-                        <p>{offer.offers.offer_4}</p>
-                        <p>{offer.offers.offer_5}</p>
-                        <p>{offer.offers.offer_6}</p>
-                        <p>----------------</p>
+                    <div 
+                        className={`offer-block ${cardHoverActive === offer.id ? 'card-active' : ''}`}
+                        key={offer.id}
+                        onMouseEnter={() => handleActiveHover(offer.id)}
+                        onMouseLeave={handleDeactiveHover}
+                    >
+                        <div className="price-block">
+                            <p>${offer.price}</p> / Monthly
+                        </div>
+
+                        <div className="offer-type">
+                            {offer.name}
+                        </div>
+
+                        <div className="offer-desc">
+                            {offer.desc}
+                        </div>
+
+                        <div className="offer-advantages-list">
+                            <ul>
+                                <li><span><FaCheck /></span> {offer.offers.offer_1}</li>
+                                <li><span><FaCheck /></span> {offer.offers.offer_2}</li>
+                                <li><span><FaCheck /></span> {offer.offers.offer_3}</li>
+                                <li><span><FaCheck /></span> {offer.offers.offer_4}</li>
+                                <li><span><FaCheck /></span> {offer.offers.offer_5}</li>
+                                <li><span><FaCheck /></span> {offer.offers.offer_6}</li>
+                            </ul>
+                        </div>
+
+                        <div className={`offer-buy-btn ${cardHoverActive === offer.id ? 'hovered' : ''}`}>
+                            <button>
+                                Buy Now
+                            </button>
+                        </div>
+                        
                     </div>
                 ))}
             </div>
