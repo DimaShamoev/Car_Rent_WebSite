f2428b77342700a5c455c0c46848a6396a60ee76
Dima Shamoev <dimashamoev5@gmail.com>
2024-11-30 17:31:52 +0400

    implement react hook form on login page

--- src/Pages/SignIn/SignIn.tsx
+++ src/Pages/SignIn/SignIn.tsx
@@ -1,21 +1,60 @@
 import React from "react";
 import "./SignIn.css";
 import { FaFacebookF, FaGoogle } from "react-icons/fa";
+import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
 
 const SignIn: React.FunctionComponent = () => {
+    interface ILoginForm {
+        email: string
+        password: string
+    }
+
+    const { register, handleSubmit, formState: {errors} } = useForm<ILoginForm>({
+        defaultValues: {}
+    })
+
+    const submit: SubmitHandler<ILoginForm> = data => {
+        console.log(data)
+    }
+
+    const error: SubmitErrorHandler<ILoginForm> = data => {
+        console.log(data)
+    }
+
     return (
         <div className="sign-in-page">
             <div className="sign-in-block">
                 <div className="sign-in-title">Sing In</div>
                 <div className="sign-in-form-block">
-                    <form>
+                    <form onSubmit={handleSubmit(submit, error)}>
                         <div className="input-block">
                             <span>Enter Mail</span>
-                            <input type="text" placeholder="example@gmail.com" />
+                            <input
+                                type="text" 
+                                placeholder="example@gmail.com" 
+                                {
+                                    ...register('email',
+                                        {
+                                            required: "This Field Is Required",
+                                            pattern: {
+                                                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                                                message: "Invalid email format",
+                                            }
+                                        })
+                                }
+                            />
+                            {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}
                         </div>
                         <div className="input-block">
                             <span>Enter Password</span>
-                            <input type="password" placeholder="********" />
+                            <input
+                                type="password"
+                                placeholder="********"
+                                {
+                                    ...register('password',
+                                    {required: "This Field Is Required"})
+                                } />
+                            {errors.password && <p style={{ color: "red" }}>{errors.password.message}</p>}
                         </div>
                         <div className="btns-block">
                             <div className="remember-btn">
--- src/Pages/SignUp/SignUp.tsx
+++ src/Pages/SignUp/SignUp.tsx
@@ -1,57 +1,75 @@
 import React from 'react'
 import './SignUp.css'
 import { FaFacebookF, FaGoogle } from 'react-icons/fa'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 const SignUp: React.FunctionComponent = () => {
-  return (
-    <div className='sign-up-page'>
-        <div className="sign-in-page">
-            <div className="sign-up-block">
-                <div className="sign-up-title">Register</div>
-                <div className="sign-up-form-block">
-                    <form>
-                        <div className="input-block">
-                            <span>Enter Username</span>
-                            <input type="text" placeholder="Usernickname" />
-                        </div>  
+    
+    interface ISignUpForm {
+        name: string
+        email: string
+        phone: string
+        password: string
+    }
 
-                        <div className="input-block">
-                            <span>Enter Mail</span>
-                            <input type="text" placeholder="example@gmail.com" />
-                        </div>
+    const {register, handleSubmit} = useForm<ISignUpForm>({
 
-                        <div className="input-block">
-                            <span>Enter Phone</span>
-                            <input type="text" placeholder="+987 654 321 000" />
-                        </div>  
+    })
 
-                        <div className="input-block">
-                            <span>Enter Password</span>
-                            <input type="password" placeholder="********" />
-                        </div>
-                        <div className="btns-block">
-                            <div className="remember-btn">
-                                <input type="checkbox" /> l Accept The Privacy Policy
+    const regSubmit: SubmitHandler<ISignUpForm> = data => {
+        console.log(data);
+    }
+
+
+    return (
+        <div className='sign-up-page'>
+            <div className="sign-in-page">
+                <div className="sign-up-block">
+                    <div className="sign-up-title">Register</div>
+                    <div className="sign-up-form-block">
+                        <form onSubmit={handleSubmit(regSubmit)}>
+                            <div className="input-block">
+                                <span>Enter Username</span>
+                                <input type="text" placeholder="Usernickname" {...register('name')} />
+                            </div>  
+
+                            <div className="input-block">
+                                <span>Enter Mail</span>
+                                <input type="text" placeholder="example@gmail.com" {...register('email')} />
                             </div>
-                        </div>
-                        <div className="sing-up-btn">
-                            <button type="submit">Register</button>
-                        </div>
-                        <div className="separate-line">OR</div>
-                        <div className="social-btns-block">
-                            <div className="facebook-btn">
-                                <button><span><FaFacebookF /></span> Continue Facebook</button>
+
+                            <div className="input-block">
+                                <span>Enter Phone</span>
+                                <input type="text" placeholder="+987 654 321 000" {...register('phone')} />
+                            </div>  
+
+                            <div className="input-block">
+                                <span>Enter Password</span>
+                                <input type="password" placeholder="********" {...register('password')} />
                             </div>
-                            <div className="google-btn">
-                                <button><span><FaGoogle /></span> Continue Google</button>
+                            <div className="btns-block">
+                                <div className="remember-btn">
+                                    <input type="checkbox" /> l Accept The Privacy Policy
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                            <div className="sing-up-btn">
+                                <button type="submit">Register</button>
+                            </div>
+                            <div className="separate-line">OR</div>
+                            <div className="social-btns-block">
+                                <div className="facebook-btn">
+                                    <button><span><FaFacebookF /></span> Continue Facebook</button>
+                                </div>
+                                <div className="google-btn">
+                                    <button><span><FaGoogle /></span> Continue Google</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default SignUp
