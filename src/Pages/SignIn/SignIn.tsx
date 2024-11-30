135b0688cf6ad5899a2fd585e41b9f4805e012a5
Dima Shamoev <dimashamoev5@gmail.com>
2024-11-30 15:40:13 +0400

    finished sing in page

--- src/Pages/SignIn/SignIn.tsx
+++ src/Pages/SignIn/SignIn.tsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import './SignIn.css'
+import React from "react";
+import "./SignIn.css";
+import { FaFacebookF, FaGoogle } from "react-icons/fa";
 
 const SignIn: React.FunctionComponent = () => {
-  return (
-    <div>SignIn Page</div>
-  )
-}
+    return (
+        <div className="sign-in-page">
+            <div className="sign-in-block">
+                <div className="sign-in-title">Sing In</div>
+                <div className="sign-in-form-block">
+                    <form>
+                        <div className="input-block">
+                            <span>Enter Mail</span>
+                            <input type="text" placeholder="example@gmail.com" />
+                        </div>
+                        <div className="input-block">
+                            <span>Enter Password</span>
+                            <input type="password" placeholder="********" />
+                        </div>
+                        <div className="btns-block">
+                            <div className="remember-btn">
+                                <input type="checkbox" /> Remember Me
+                            </div>
+                            <div className="forget-pass-btn">
+                                <a href="#">Forget Password</a>
+                            </div>
+                        </div>
+                        <div className="sing-in-btn">
+                            <button type="submit">Sign In</button>
+                        </div>
+                        <div className="separate-line">OR</div>
+                        <div className="social-btns-block">
+                            <div className="facebook-btn">
+                                <button><span><FaFacebookF /></span> Continue Facebook</button>
+                            </div>
+                            <div className="google-btn">
+                                <button><span><FaGoogle /></span> Continue Google</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default SignIn
+export default SignIn;
